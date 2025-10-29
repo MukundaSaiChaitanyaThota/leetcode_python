8bc5728c6eb679db45e4151a503a639a3d2444ee
MukundaSaiChaitanyaThota <chaitanyatms675@gmail.com>
2025-10-29 19:00:10 +0530

    updated summary and function call

--- problems/Hash_Sets_Maps/Jewels_and_stones.py
+++ problems/Hash_Sets_Maps/Jewels_and_stones.py
@@ -1,6 +1,11 @@
 # 771. Jewels and Stones
+# Given strings jewels and stones, where jewels represents the types of stones that are jewels, and stones represents the stones you have. 
+# Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+# Letters are case sensitive, so "a" is considered a different type of stone from "A".
 
-def numJewelsInStones(self, jewels: str, stones: str) -> int:
+# time complexity: O(n + m)
+# space complexity: O(m)
+def numJewelsInStones( jewels: str, stones: str) -> int:
     stones_dict={}
     result=0
     for char in stones:
@@ -12,3 +17,12 @@ def numJewelsInStones(self, jewels: str, stones: str) -> int:
         if char in stones_dict.keys():
             result+=stones_dict[char]
     return result
+
+if __name__ == "__main__":
+    jewels = "aA"
+    stones = "aAAbbbb"
+    print(numJewelsInStones(jewels, stones))  # Output: 3
+
+    jewels = "z"
+    stones = "ZZ"
+    print(numJewelsInStones(jewels, stones))  # Output: 0
