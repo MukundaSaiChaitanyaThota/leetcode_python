af7723c3f255486a61f0f88b7765abc0dd02d261
MukundaSaiChaitanyaThota <chaitanyatms675@gmail.com>
2025-12-28 23:51:09 +0530

    Implementing binary search to find the insert position of a target value in a sorted array.

---
+++ Binary_search/insert-position.py
@@ -0,0 +1,26 @@
+# 35. Search Insert Position
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+# # You must write an algorithm with O(log n) runtime complexity.
+
+
+from typing import List
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        l = 0
+        r = n - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] < target:
+                l = m + 1
+            elif nums[m] > target:
+                r = m - 1
+            else:
+                return m
+        if nums[m] < target:
+            return m + 1
+        else:
+            return m
+
+    
